6316a7a027ab33d1236bf7056ce5fc8596070992
Pinetree <pine@woodlarch.com>
2026-08-21 18:41:36 -0400

    Removed defines related to windows and mac

--- src/crosshair.c
+++ src/crosshair.c
@@ -7,12 +7,8 @@
 #include <string.h>
 
 // Tray
-#if defined (_WIN32) || defined (_WIN64)
-#define TRAY_WINAPI 1
-#elif defined (__linux__) || defined (linux) || defined (__linux)
+#if defined (__linux__) || defined (linux) || defined (__linux)
 #define TRAY_APPINDICATOR 1
-#elif defined (__APPLE__) || defined (__MACH__)
-#define TRAY_APPKIT 1
 #endif
 
 #include "tray/tray_linux.c"
@@ -20,12 +16,6 @@
 #if TRAY_APPINDICATOR
 #define TRAY_ICON1 "indicator-messages-new"
 #define TRAY_ICON2 "indicator-messages-new"
-#elif TRAY_APPKIT
-#define TRAY_ICON1 "icon.png"
-#define TRAY_ICON2 "icon.png"
-#elif TRAY_WINAPI
-#define TRAY_ICON1 "icon.ico"
-#define TRAY_ICON2 "icon.ico"
 #endif
 
 static struct tray tray;
